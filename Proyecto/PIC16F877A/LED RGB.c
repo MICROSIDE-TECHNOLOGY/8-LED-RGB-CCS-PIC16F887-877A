c8863270666f1422a4ec8a8a8f7c65d96f6df2ae
vhperaltaj <victor.peralta@microside.com>
2023-08-29 16:52:46 -0600

    Updated indentantion, added gitignore

--- Proyecto/PIC16F877A/LED RGB.c
+++ Proyecto/PIC16F877A/LED RGB.c
@@ -1,7 +1,12 @@
-/* 
- AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
- FECHA: JUNIO 2019
-*/
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+LED RGB.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
 /*
 ---------------------------------------------------------------------------
@@ -10,72 +15,48 @@ para el control de un LED RGB
 ---------------------------------------------------------------------------
 */
 
-#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F877A.h>                     //Incluye el microcontrolador con el que se va a trabajar 
+#use delay(clock=20Mhz, crystal)         //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+#define LEDR PIN_B0                      //Pin donde está conectado el LED rojo
+#define LEDV PIN_B1                      //Pin donde está conectado el LED verde
+#define LEDA PIN_B2                      //Pin donde está conectado el LED azul
 
-#define LEDR PIN_B0                             //Pin donde está conectado el LED rojo
-#define LEDV PIN_B1                             //Pin donde está conectado el LED verde
-#define LEDA PIN_B2                             //Pin donde está conectado el LED azul
+unsigned int16 Duty = 0;                 //Variable para guardar el valor para PWM
 
-unsigned int16 Duty=0;                          //Variable para guardar el valor para PWM
-
-void SisInit(void)
-
-{
-   setup_timer_2 (T2_DIV_BY_16, 254, 1);                        //Configura Timer2, periodo ajustado a 342uS
-   setup_ccp1 (CCP_PWM);    //Configura el módulo CCP para uso del PWM
-}
-
-void Set_PWM (void)
-
-{
-
-  // int i;                                                       //Declaramos variable
-   for (Duty = 255; Duty > 0; Duty--)                             // Ciclo para disminuir el valor de la variable
-
-   {
-    set_pwm1_duty (Duty);                                         // Guarda la salida PWM en la variable
-    delay_ms (5);                                                 // Retardo
-    }
-
-    for (Duty = 0; Duty < 255; Duty++)                            // Ciclo para aumentar el valor de la variable
-
-    {
-     set_pwm1_duty (Duty);                                        // Guarda la salida PWM en la variable
-     delay_ms (5);                                                // Retardo
+void Set_PWM ( void ) {
+   for (Duty = 255; Duty > 0; Duty--) {  // Ciclo para disminuir el valor de la variable
+      set_pwm1_duty (Duty);              // Guarda la salida PWM en la variable
+      delay_ms (5);                      // Retardo
    }
 
+   for (Duty = 0; Duty < 255; Duty++) {  // Ciclo para aumentar el valor de la variable
+      set_pwm1_duty (Duty);              // Guarda la salida PWM en la variable
+      delay_ms (5);                      // Retardo
+   }
 }
 
-void main()
+void main ( void ) {
+   setup_timer_2 (T2_DIV_BY_16, 254, 1); //Configura Timer2, periodo ajustado a 342uS
+   setup_ccp1 (CCP_PWM);                 //Configura el módulo CCP para uso del PWM
 
-{
+    while ( 1 ) {                        // Ciclo infinito
+       output_high ( LEDR );
+       output_low ( LEDV );
+       output_low ( LEDA );
 
-   SisInit();
+       Set_PWM ( );                      // Convocamos la función
 
-    while (1)                                                   // Ciclo infinito
+       output_low ( LEDR );
+       output_high ( LEDV );
+       output_low ( LEDA );
 
-     {
+       Set_PWM ( );
 
-       output_high (LEDR) ;
-       output_low (LEDV) ;
-       output_low (LEDA) ;
+       output_low ( LEDR );
+       output_low ( LEDV );
+       output_high ( LEDA );
 
-       Set_PWM ();                                              // Convocamos la función
-
-       output_low (LEDR) ;
-       output_high (LEDV) ;
-       output_low (LEDA) ;
-
-       Set_PWM ();                                              // Convocamos la función
-
-       output_low (LEDR) ;
-       output_low (LEDV) ;
-       output_high (LEDA) ;
-
-       Set_PWM ();                                              // Convocamos la función
-
+       Set_PWM ( );
      }
-
 }
